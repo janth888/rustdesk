7e8e5b90415708f735fffb29f5d34c555ff2c2a7
janth888 <45552296+janth888@users.noreply.github.com>
2024-10-04 11:57:24 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.leox.ink"];
-pub const PUBLIC_RS_PUB_KEY: &str = "BWd65SnWkS330O4FiE+YoBeTOcmWnG6sEv7RZ1FXejQ=";
+pub const PUBLIC_RS_PUB_KEY: &str = "wIxd1PLkhXLh3lEt2tNFiGA5NKBRiMFtMTJTTdgkXdE=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
